23666a5892c792dc063e5d480e0cdd2ef0f66771
jp41011 <jp41011@users.noreply.github.com>
2026-08-28 00:31:21 -0700

    Change gitcommit colorscheme to retrobox

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -2,4 +2,4 @@ local set = vim.opt_local
 
 set.colorcolumn = '50,72'
 
-vim.cmd 'colorscheme rose-pine-main'
+vim.cmd 'colorscheme retrobox'
--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -14,14 +14,14 @@ return {
       vim.cmd 'colorscheme onedark_vivid'
     end,
   },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    --config = function()
-    -- vim.cmd 'colorscheme rose-pine'
-    -- vim.cmd 'colorscheme rose-pine-main'
-    --vim.cmd 'colorscheme rose-pine-moon'
-    -- vim.cmd 'colorscheme rose-pine-dawn'
-    --end,
-  },
+  --{
+  --  'rose-pine/neovim',
+  --  name = 'rose-pine',
+  --  --config = function()
+  --  -- vim.cmd 'colorscheme rose-pine'
+  --  -- vim.cmd 'colorscheme rose-pine-main'
+  --  --vim.cmd 'colorscheme rose-pine-moon'
+  --  -- vim.cmd 'colorscheme rose-pine-dawn'
+  --  --end,
+  --},
 }
